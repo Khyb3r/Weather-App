99ed5c1a46287d689c2d149e685d0481ce0aa815
Khyb3r <157541714+Khyb3r@users.noreply.github.com>
2024-10-02 02:22:49 +0100

    Comments finished
    WeatherApp and App Launcher classes finished

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -2,9 +2,12 @@ import javax.swing.*;
 
 public class AppLauncher {
     public static void main(String[] args) {
+
+        // Allows thread safe behaviours for the GUI
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
+                // Instantiated Weather App
                 new WeatherAppGUI();
             }
         });
